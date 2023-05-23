3c8bc1f0b232ccd4d14daa2b4d6a352dd8c043e0
William Mtemi Mponzi <willbrian009@gmail.com>
2023-05-23 22:11:53 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 5, 'F', 'White', 'Calm', 1),
+  ('Rowdy', 1, 'M', 'bLUE', 'Playful', 1),
+  ('Tabitha', 7, 'M', 'Tabby', 'Playful', 1),
+  ('Sergeant Brown', 3, 'M', 'Tabby', 'Playful', 1),
+  ('Melissa', 2, 'F', 'Tabby', 'Playful', 1),
+  ('Grinch', 8, 'M', 'Tabby', 'Playful', 1),
+  ('Wendy', 6, 'F', 'Tabby', 'Playful', 1),
+  (NULL, 1, 'F', 'Tabby', 'Playful', 1);
